3b5356fec0e299c895e8f6abfe38bf33c6fa7da0
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-05-02 11:36:58 +0000

    feat: installed ngrx/effects

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
+import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideStore()]
+  providers: [provideRouter(routes), provideClientHydration(), provideStore(), provideEffects()]
 };
