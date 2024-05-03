baa4b0df7132f5272b733c606a851bd3a0dfc525
meet-tim <kumahtim18@gmail.com>
2024-05-03 12:08:17 +0000

    added port number

--- backend-api/src/main.ts
+++ backend-api/src/main.ts
@@ -4,7 +4,8 @@ import { ValidationPipe } from '@nestjs/common/pipes';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT,'0.0.0.0');
+  await app.listen(port,'0.0.0.0');
 }
 bootstrap();
