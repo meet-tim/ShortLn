dc75f8068ba8021bf69c947738c0edc38fc7af32
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-05-04 13:49:09 +0000

    feat: added spartan ui input element

--- frontend/src/app/views/sign-in/sign-in.component.ts
+++ frontend/src/app/views/sign-in/sign-in.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
   imports: [],
   templateUrl: './sign-in.component.html',
-  styleUrl: './sign-in.component.css'
+  styleUrl: './sign-in.component.css',
 })
 export class SignInComponent {
+  formBuilder = inject(FormBuilder);
 
+  singInForm = this.formBuilder.group({
+    email: [''],
+    password: [''],
+  });
 }
