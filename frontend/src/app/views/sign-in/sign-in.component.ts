5f3d407648c527a219ec11d010bc10e2c116d637
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-05-06 09:22:43 +0000

    feat: added input validation to the sign-in page

--- frontend/src/app/views/sign-in/sign-in.component.ts
+++ frontend/src/app/views/sign-in/sign-in.component.ts
@@ -1,18 +1,68 @@
-import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { HlmInputDirective } from '../../core/components/ui-input-helm/src/lib/hlm-input.directive';
+import { Subscription } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { HlmLabelDirective } from '../../core/components/ui-label-helm/src/lib/hlm-label.directive';
+import { HlmCheckboxComponent } from '../../core/components/ui-checkbox-helm/src/lib/hlm-checkbox.component';
+import { HlmButtonDirective } from '../../core/components/ui-button-helm/src/lib/hlm-button.directive';
 
 @Component({
   selector: 'app-sign-in',
   standalone: true,
-  imports: [],
+  imports: [
+    HlmInputDirective,
+    ReactiveFormsModule,
+    CommonModule,
+    HlmLabelDirective,
+    HlmCheckboxComponent,
+    HlmButtonDirective,
+  ],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.css',
 })
-export class SignInComponent {
+export class SignInComponent implements OnInit, OnDestroy {
   formBuilder = inject(FormBuilder);
 
   singInForm = this.formBuilder.group({
-    email: [''],
-    password: [''],
+    email: [
+      '',
+      [Validators.required, Validators.email],
+      { updateOn: 'submit' },
+    ],
+    password: ['', [Validators.required]],
   });
+  signInFormSubscription!: Subscription;
+  checkboxValue = false;
+
+  ngOnInit(): void {
+    this.signInFormSubscription = this.singInForm.valueChanges.subscribe(
+      (value) => {
+        console.log({ rememberMe: this.checkboxValue, ...value });
+      }
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.signInFormSubscription.unsubscribe();
+  }
+
+  onCheckboxChange(event: unknown) {
+    this.checkboxValue = event as boolean;
+    this.singInForm.updateValueAndValidity();
+  }
+
+  onFormSubmit(event: SubmitEvent) {
+    event.preventDefault();
+    if (
+      this.singInForm.controls.email.invalid &&
+      this.singInForm.controls.password.invalid
+    ) {
+      console.log(this.singInForm.controls.email.invalid);
+      console.log(this.singInForm.controls.password.invalid);
+      this.singInForm.markAllAsTouched();
+      return;
+    }
+    console.log({ rememberMe: this.checkboxValue, ...this.singInForm.value });
+  }
 }
