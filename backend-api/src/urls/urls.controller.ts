8a9a5d82c57eee6a569c32fa5ccb3f8cdabbcc43
meet-tim <kumahtim18@gmail.com>
2024-05-07 15:07:32 +0000

    Added delete endpoint

--- backend-api/src/urls/urls.controller.ts
+++ backend-api/src/urls/urls.controller.ts
@@ -1,18 +1,29 @@
-import { Body, Controller,Delete,Post } from '@nestjs/common';
+import { Body, Controller,Delete,Get,Param,Post } from '@nestjs/common';
 import { UrlsService } from './urls.service';
+import { Url } from './schemas/urls.schema';
 
 @Controller('urls')
 export class UrlsController {
 
     constructor(private readonly urlsService: UrlsService) {}
 
+    @Get('all')
+    async getUrls():Promise<Url[]>{
+        return this.urlsService.findAll();
+    }
     @Post('shorten')
     async shortenUrl(@Body('url') url: string): Promise<string> {
       const shortUrl = await this.urlsService.shortenUrl(url);
       return `https://shortln-production.up.railway.app/${shortUrl.shortUrl}`;
     }
 
-    @Delete()
+    @Delete(':id')
+    async deleteUrl(@Param('id') id: string): Promise<string>{
+        await this.urlsService.deleteUrl(id);
+        return "success";
+    }
+
+
   /*
     @Post('expand')
     async expandUrl(@Body('shortUrl') shortUrl: string): Promise<string> {
--- backend-api/src/urls/urls.service.ts
+++ backend-api/src/urls/urls.service.ts
@@ -6,28 +6,32 @@ import { Model } from 'mongoose';
 
 @Injectable()
 export class UrlsService {
-    constructor(@InjectModel("Url") private readonly userModel: Model<Url>) {}
+    constructor(@InjectModel("Url") private readonly urlModel: Model<Url>) {}
 
     async findAll(): Promise<Url[]> {
-      return this.userModel.find().exec();
+      return this.urlModel.find().exec();
     }
 
     async shortenUrl(url:string): Promise<Url>{
         const urlObj = new Url();
         urlObj.originalUrl = url;
         urlObj.shortUrl = this.generateCode(5)
-        const createdUrl = new this.userModel(urlObj)
+        const createdUrl = new this.urlModel(urlObj)
         return createdUrl.save();
     }
 
-      private  generateCode(length) {
+    async deleteUrl(id: string): Promise<Url | null> {
+        return this.urlModel.findOneAndDelete({ urlId: id }).exec();
+    }
+
+    private  generateCode(length) {
         let result = '';
         const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
         const charactersLength = characters.length;
         let counter = 0;
         while (counter < length) {
-          result += characters.charAt(Math.floor(Math.random() * charactersLength));
-          counter += 1;
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
         }
         return result;
     }
