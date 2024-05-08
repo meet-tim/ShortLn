21fc11ebc4627315e00ab254d79e7e735e4a3f02
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-05-08 12:07:28 +0000

    feat: added header with avartar and company logo

--- frontend/src/app/views/links/links.component.ts
+++ frontend/src/app/views/links/links.component.ts
@@ -1,14 +1,15 @@
-import { CommonModule, isPlatformBrowser } from '@angular/common';
-import { Component, PLATFORM_ID, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, NgZone, inject } from '@angular/core';
 import { injectQuery } from '@tanstack/angular-query-experimental';
 import { HlmButtonDirective } from '../../core/components/ui-button-helm/src/lib/hlm-button.directive';
 import { UserService } from '../../core/services/user/user.service';
 import { UserStore } from '../../core/store/user/user.store';
+import { HlmAvatarComponent } from '../../core/components/ui-avatar-helm/src/lib/hlm-avatar.component';
 
 @Component({
   selector: 'app-links',
   standalone: true,
-  imports: [HlmButtonDirective, CommonModule],
+  imports: [HlmButtonDirective, CommonModule, HlmAvatarComponent],
   providers: [],
   templateUrl: './links.component.html',
   styleUrl: './links.component.css',
@@ -16,19 +17,25 @@ import { UserStore } from '../../core/store/user/user.store';
 export class LinksComponent {
   userService = inject(UserService);
   userStore = inject(UserStore);
+  ngZone = inject(NgZone);
 
-  profileQuery =
-    isPlatformBrowser(inject(PLATFORM_ID)) &&
+  profileQuery = this.ngZone.runOutsideAngular(() =>
     injectQuery(() => ({
       queryKey: ['user-profile'],
       queryFn: async () => {
         try {
           const data = await this.userService.fetchUserProfile();
-
-          return await this.userService.fetchUserProfile();
+          return this.ngZone.run(() => {
+            this.userStore.setUserNameAndEmail({
+              email: data.email,
+              name: data.sub,
+            });
+            return data;
+          });
         } catch (error: unknown) {
           throw new Error('Failed to fetch user profile');
         }
       },
-    }));
+    }))
+  );
 }
