1ce01dcb3f0c45948b8921676a4c75475239728a
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-05-08 15:22:13 +0000

    feat: designed the links page

--- frontend/src/app/views/links/links.component.ts
+++ frontend/src/app/views/links/links.component.ts
@@ -1,76 +1,18 @@
-import { CommonModule } from '@angular/common';
-import { Component, NgZone, inject } from '@angular/core';
-import { injectQuery } from '@tanstack/angular-query-experimental';
-import { HlmButtonDirective } from '../../core/components/ui-button-helm/src/lib/hlm-button.directive';
-import { UserService } from '../../core/services/user/user.service';
-import { UserStore } from '../../core/store/user/user.store';
-import { HlmAvatarComponent } from '../../core/components/ui-avatar-helm/src/lib/hlm-avatar.component';
-import {
-  HlmMenuComponent,
-  HlmMenuGroupComponent,
-  HlmMenuItemDirective,
-  HlmMenuLabelComponent,
-  HlmMenuSeparatorComponent,
-} from '@spartan-ng/ui-menu-helm';
-import { BrnMenuTriggerDirective } from '@spartan-ng/ui-menu-brain';
-import { AuthService } from '../../core/services/auth/auth.service';
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../components/header/header.component';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { HlmSwitchComponent } from '@spartan-ng/ui-switch-helm';
 
 @Component({
   selector: 'app-links',
   standalone: true,
-  imports: [
-    HlmButtonDirective,
-    CommonModule,
-    HlmAvatarComponent,
-    BrnMenuTriggerDirective,
-    HlmMenuComponent,
-    // HlmSubMenuComponent,
-    HlmMenuItemDirective,
-    // HlmMenuItemSubIndicatorComponent,
-    HlmMenuLabelComponent,
-    // HlmMenuShortcutComponent,
-    HlmMenuSeparatorComponent,
-    // HlmMenuItemIconDirective,
-    // HlmMenuItemCheckComponent,
-    // HlmMenuItemRadioComponent,
-    HlmMenuGroupComponent,
-    // HlmMenuItemRadioDirective,
-    // HlmMenuItemCheckboxDirective,
-
-    // HlmButtonDirective,
-    // HlmIconComponent,
-  ],
+  imports: [HeaderComponent, HlmLabelDirective, HlmSwitchComponent],
   providers: [],
   templateUrl: './links.component.html',
   styleUrl: './links.component.css',
 })
 export class LinksComponent {
-  userService = inject(UserService);
-  userStore = inject(UserStore);
-  ngZone = inject(NgZone);
-  authService = inject(AuthService);
-
-  profileQuery = this.ngZone.runOutsideAngular(() =>
-    injectQuery(() => ({
-      queryKey: ['user-profile'],
-      queryFn: async () => {
-        try {
-          const data = await this.userService.fetchUserProfile();
-          return this.ngZone.run(() => {
-            this.userStore.setUserNameAndEmail({
-              email: data.email,
-              name: data.sub,
-            });
-            return data;
-          });
-        } catch (error: unknown) {
-          throw new Error('Failed to fetch user profile');
-        }
-      },
-    }))
-  );
-
-  onSignOutButtonClick() {
-    this.authService.singOut();
+  onCheckboxChange(value: boolean) {
+    console.log(value);
   }
 }
