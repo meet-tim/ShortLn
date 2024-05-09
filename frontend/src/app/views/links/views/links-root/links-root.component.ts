7ac9f15c4b13a0e788dd8ccaa6045abfb6a3db98
Konadu-Akwasi-Akuoko <konaduakwasiakuoko@gmail.com>
2024-05-09 12:15:29 +0000

    feat: added the code at the root of the links route to the roots-links compoents

--- frontend/src/app/views/links/views/links-root/links-root.component.ts
+++ frontend/src/app/views/links/views/links-root/links-root.component.ts
@@ -1,12 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, WritableSignal, signal } from '@angular/core';
+import { HlmButtonDirective } from '@spartan-ng/ui-button-helm';
+import { HlmLabelDirective } from '@spartan-ng/ui-label-helm';
+import { HlmSwitchComponent } from '@spartan-ng/ui-switch-helm';
+import { toast } from 'ngx-sonner';
 
 @Component({
   selector: 'app-links-root',
   standalone: true,
-  imports: [],
+  imports: [HlmLabelDirective, HlmSwitchComponent, HlmButtonDirective],
   templateUrl: './links-root.component.html',
-  styleUrl: './links-root.component.css'
+  styleUrl: './links-root.component.css',
 })
 export class LinksRootComponent {
+  links: WritableSignal<{ longUrl: string; shortenedUrl: string }[]> = signal([
+    {
+      longUrl: 'https://www.konadu.dev/sdaklf;dsfjksdafsd;kffalfsdfsadklfj',
+      shortenedUrl: 'https://www.konadu.dev',
+    },
+    {
+      longUrl: 'https://www.konadu.dev/sdaklf;dsfjksdafsd;kffalfsdfsadklfj',
+      shortenedUrl: 'https://www.konadu.dev',
+    },
+    {
+      longUrl: 'https://www.konadu.dev/sdaklf;dsfjksdafsd;kffalfsdfsadklfj',
+      shortenedUrl: 'https://www.konadu.dev',
+    },
+  ]);
 
+  onCheckboxChange(value: boolean) {
+    console.log(value);
+  }
+
+  onDeleteClick() {
+    this.links.update((prev) => prev.slice(0, prev.length - 1));
+    toast('Link deleted', {
+      description: 'Link has been deleted successfully',
+      action: {
+        label: 'Ok',
+        onClick: () => null,
+      },
+    });
+  }
+
+  onCopyClick() {
+    toast('Link copied', {
+      description: 'Link has been copied to clipboard',
+      action: {
+        label: 'Ok',
+        onClick: () => null,
+      },
+    });
+  }
 }
