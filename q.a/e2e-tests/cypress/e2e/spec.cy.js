ccd9ed0669f2ede3d4d137c977441962eee3fdfa
Doricy <doricyj@gmail.com>
2024-05-10 12:02:36 +0000

    added the login e2e tests to the sign up tests

--- q.a/e2e-tests/cypress/e2e/spec.cy.js
+++ q.a/e2e-tests/cypress/e2e/spec.cy.js
@@ -1,8 +1,20 @@
-describe('Login functionality on shortln website', () => {
+describe('Shortln app tests', () => {
+  it('should allow user to sign up with email, first name, last name, and password', () => {
+    cy.visit('https://shortln.konadu.dev/sign-up');
+    // Fill in sign up form fields
+    cy.get('#firstname').type('Doris');
+    cy.get('#lastname').type('Gyebi');
+    cy.get('#email').type('nananana@gmail.com');
+    cy.get('#password').type('nana123');
+    
+    // Click on sign up button
+    cy.get('button[type="submit"]').click();
+  });
+
+
   it('should login successfully with valid email and password', () => {
 
-    // Visit the login page before each test
-    cy.visit('https://shortln.konadu.dev/sign-in');
+    cy.visit('https://shortln.konadu.dev/sign-in'); // Visit the login page
 
     //assert that the login form elements (email and password input fields are visible.
     cy.get('#email').should('be.visible');
@@ -13,21 +25,12 @@ describe('Login functionality on shortln website', () => {
     cy.get('#password').type('doris1234');
     
     // Click on login button
-    cy.get('.ng-submitted > :nth-child(4)').click();
+    cy.get('.bg-blue-500').click();
 
     // Assert that the user is redirected to the home page
-    cy.url().should('include', '/https://shortln.konadu.dev/links');
+    cy.url().should('include', '/links');
     
   });
-
-
-
-  it('should redirect to sign up page when clicking on sign up', () => {
-    // Click on sign up link
-    cy.get('.text-red-500').click();
-
-    // Assert that the user is redirected to the sign up page
-    cy.url().should('include', '/https://shortln.konadu.dev/sign-up');
-  });
+  
 })
   
